d3e6bb15d5eb3d2fd522c47fe34467d0a04010b2
Muhammad Umair Ali Shah <67957019+umair-ali-shah@users.noreply.github.com>
2025-02-20 07:05:31 +0000

    Created Initial Draft of CMSC Website

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return "Hello Usman!!"
 
-if __name__ == '__main__': # Makes sure this is the main process by python app.py 
+@app.route('/')
+def home():
+  return render_template('cmsc.html')
+
+
+if __name__ == '__main__':  # Makes sure this is the main process by python app.py
   app.run(host='0.0.0.0', debug=True)
